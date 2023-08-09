c0161ddb26e22d5c07a20ff57ce1926ce450ca65
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-08-08 21:05:26 -0400

    updates

--- server/models.py
+++ server/models.py
@@ -1,10 +1,9 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
 from config import db
 
 class User(db.Model):
-    __tablename__ = 'users'
+    tablename = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
@@ -12,25 +11,23 @@ class User(db.Model):
 
 
 class Bathroom(db.Model):
-    __tablename__ = 'bathrooms'
+    tablename = 'bathrooms'
 
     id = db.Column(db.Integer, primary_key=True)
     bathroom_name = db.Column(db.String)
-    reviews = db.Column(db.String, db.ForeignKey('reviews.id')) 
     street_num = db.Column(db.Integer)
     street_name = db.Column(db.String)
     city = db.Column(db.String)
     zip_code = db.Column(db.Integer)
 
+    reviews = db.relationship('Review', backref='bathroom')
 
 
 class Review(db.Model):
-    __tablename__ = 'reviews'
+    tablename = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String, db.ForeignKey('users.id')) 
-    bathroom_id = db.Column(db.String, db.ForeignKey('bathrooms.id')) 
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+    bathroom_id = db.Column(db.Integer, db.ForeignKey('bathrooms.id')) 
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, update=db.func.now())
-
-  
+    # updated_at = db.Column(db.DateTime, onupdate=db.func.now())
