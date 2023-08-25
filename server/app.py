e22c59e8c65b561e27db26ec809ff3b96f74cf0a
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-08-25 12:29:20 -0400

    creating endpoints for reviews

--- server/app.py
+++ server/app.py
@@ -42,10 +42,52 @@ class Bathrooms(Resource):
 
 class Reviews(Resource):
     def get(self):
-        review = [review.to_dict() for review in Review.query.all()]
-        return make_response(review, 200)
+        reviews = [review.to_dict() for review in Review.query.all()]
+        return make_response(reviews, 200)
 
-        
+    def post(self):
+        request_json = request.get_json()
+
+        content = request_json.get('content')
+        user_id = request_json.get('user_id')
+        bathroom_id = request_json.get('bathroom_id')
+
+        review = Review(
+            content=content,
+            user_id=user_id,
+            bathroom_id=bathroom_id
+        )
+
+        db.session.add(review)
+        db.session.commit()
+
+        return make_response({'message': 'Review created successfully'}, 201)
+
+    def put(self, review_id):
+        request_json = request.get_json()
+
+        review = Review.query.get(review_id)
+        if not review:
+            return make_response({'message': 'Review not found'}, 404)
+
+        # Update the review attributes based on the request JSON
+        review.content = request_json.get('content')
+
+        db.session.commit()
+
+        return make_response({'message': 'Review updated successfully'}, 200)
+
+    def delete(self, review_id):
+        review = Review.query.get(review_id)
+        if not review:
+            return make_response({'message': 'Review not found'}, 404)
+
+        db.session.delete(review)
+        db.session.commit()
+
+        return make_response({'message': 'Review deleted successfully'}, 200)
+
+    
 
 @app.route('/') #home route to RR site
 def index():
@@ -61,14 +103,42 @@ def get_bathrooms_by_zip_code(zip_code):
     bathrooms = Bathroom.query.filter_by(zip_code=zip_code).all()
     return [bathroom.to_dict() for bathroom in bathrooms]
 
-@app.route('/reiews', methods=['GET']) #master list of reviews
+@app.route('/reviews', methods=['GET']) #master list of reviews
 def get_reviews():
     all_reviews = Review.query.all()
     return [review.to_dict() for review in all_reviews]
 
-# @app.route('/bathroom') 
-# api.add_resource(Users, '/users', endpoint ='users')
-# api.add_resource(Bathroom, '/bathroom', endpoint='bahtroom')
+
+@app.route('/reviews/<int:review_id>', methods=['PUT'])  # update a review
+def update_review(review_id):
+    review = Review.query.get(review_id)
+    if not review:
+        return make_response({'message': 'Review not found'}, 404)
+
+    request_json = request.get_json()
+    new_content = request_json.get('content')
+
+    review.content = new_content
+    db.session.commit()
+
+    return make_response({'message': 'Review updated successfully'}, 200)
+
+@app.route('/reviews/<int:review_id>', methods=['DELETE'])  # delete a review
+def delete_review(review_id):
+    review = Review.query.get(review_id)
+    if not review:
+        return make_response({'message': 'Review not found'}, 404)
+
+    db.session.delete(review)
+    db.session.commit()
+
+    return make_response({'message': 'Review deleted successfully'}, 200)
+
+
+#function is used to associate your resource classes with specific URLs.
+api.add_resource(Users, '/users')
+api.add_resource(Bathrooms, '/bathrooms')
+api.add_resource(Reviews, '/reviews')
 
 
 #need route to create & read,possibly update user
